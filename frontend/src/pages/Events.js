d40eeda6870c6c009bde0546fff71472c78bfe35
Elvan Hidayat <elvanhd@gmail.com>
2023-12-13 07:54:13 +0700

    returning responses in a loader

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -3,7 +3,8 @@ import { useLoaderData } from 'react-router-dom';
 import EventsList from '../components/EventsList';
 
 function EventsPage() {
-  const events = useLoaderData();
+  const data = useLoaderData();
+  const events = data.events;
 
   return <EventsList events={events} />;
 }
@@ -16,7 +17,6 @@ export async function loader() {
   if (!response.ok) {
     // ...
   } else {
-    const resData = await response.json();
-    return resData.events;
+    return response;
   }
 }
--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -3,10 +3,13 @@ import { Outlet } from 'react-router-dom';
 import MainNavigation from '../components/MainNavigation';
 
 function RootLayout() {
+  // const navigation = useNavigation();
+
   return (
     <>
       <MainNavigation />
       <main>
+        {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
         <Outlet />
       </main>
     </>
