610c45ffc7223623c3a5e9a50edf448b9e85f3f3
Elvan Hidayat <elvanhd@gmail.com>
2023-12-14 23:58:27 +0700

    defering data fetching

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,11 @@
+import PageContent from '../components/PageContent';
+
 function HomePage() {
-  return <h1>HomePage</h1>;
+  return (
+    <PageContent title='Welcome!'>
+      <p>Browse all our amazing events!</p>
+    </PageContent>
+  );
 }
 
 export default HomePage;
